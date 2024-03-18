e271631c2b53b3839d7034bcbe3ff0b9991405ca
jblanked <82678820+jblanked@users.noreply.github.com>
2024-03-17 23:25:44 -0400

    Update EasySCADE.swift

--- Sources/EasySCADE/EasySCADE.swift
+++ Sources/EasySCADE/EasySCADE.swift
@@ -141,11 +141,11 @@ private func Rectangle(
     for (index, line) in lines.enumerated() {
         let svgText = SCDSvgText()
         svgText.text = line
-        //svgText.x = 0
-        //svgText.y = SCDSvgUnit(integerLiteral: Int(25 + (index * 30))) // Adjust y based on line number
+        svgText.x = Int(screenInfo.screenSize.width) / 2 
+        svgText.y = SCDSvgUnit(integerLiteral: Int(25 + (index * 30))) // Adjust y based on line number
         svgText.fill = SCDSvgColor.white
         svgText.fontSize = 20
-        svgText.anchor = SCDSvgTextAnchor.start
+        svgText.anchor = SCDSvgTextAnchor.middle
         svgText.alignment = SCDSvgTextAlignment.center
         svgText.alignmentBaseline = SCDSvgTextAlignmentBaseline.middle
         svgText.fontName = 	"ArialMT"
