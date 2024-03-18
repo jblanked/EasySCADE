5a9d4c8adfdca7de7a209a180c6b580bfa65f916
jblanked <82678820+jblanked@users.noreply.github.com>
2024-03-17 23:26:29 -0400

    Update EasySCADE.swift

--- Sources/EasySCADE/EasySCADE.swift
+++ Sources/EasySCADE/EasySCADE.swift
@@ -141,7 +141,7 @@ private func Rectangle(
     for (index, line) in lines.enumerated() {
         let svgText = SCDSvgText()
         svgText.text = line
-        svgText.x = Int(screenInfo.screenSize.width) / 2 
+        svgText.x = SCDSvgUnit(integerLiteral:Int(screenInfo.screenSize.width) / 2)
         svgText.y = SCDSvgUnit(integerLiteral: Int(25 + (index * 30))) // Adjust y based on line number
         svgText.fill = SCDSvgColor.white
         svgText.fontSize = 20
