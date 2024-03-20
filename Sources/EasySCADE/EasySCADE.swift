56341211da296932435a2ede1da637c60d621124
jblanked <82678820+jblanked@users.noreply.github.com>
2024-03-20 18:32:03 -0400

    Update EasySCADE.swift

--- Sources/EasySCADE/EasySCADE.swift
+++ Sources/EasySCADE/EasySCADE.swift
@@ -888,20 +888,34 @@ public func EasySCDCheckbox(
   }
 
 // creates a SCDCheckboxElement (label + checkbox)
-public func EasySCDCheckboxElement(text: String, fontColor: SCDGraphicsRGB = EasyColor.black, checked: Bool = false, height: Int = 50, action: @escaping () -> Void = { }) -> SCDWidgetsContainer
-  {
-  	let container = SCDWidgetsContainer()
-  	let label = EasySCDTextLabel(text: text, fontsize: 20, font: "ArialMT", fontcolor: fontColor, paddingVertical: 0, paddingHorizontal: 10, x_location: 0, y_location: 10)
-  	let checkbox = EasySCDCheckbox(checked: checked, height: height)
-	checkbox.location = SCDGraphicsPoint(x: Int(screenInfo.screenSize.width) - height - 20, y: 0)
-	checkbox.size = SCDGraphicsDimension(width: Int(screenInfo.screenSize.width), height: height)
-	container.children.append(label)
-  	container.children.append(checkbox)
-  	container.size = SCDGraphicsDimension(width: Int(screenInfo.screenSize.width), height: height)
-	container.location = SCDGraphicsPoint(x: 0, y: 0)
-  	container.onClick { _ in action() }
-  	return container
-  }
+public func EasySCDCheckboxElement(
+    text: String, 
+    fontColor: SCDGraphicsRGB = EasyColor.black, 
+    checked: Bool = false, 
+    height: Int = 50, 
+    action: @escaping () -> Void = { },
+    onCheckedChanged: @escaping (Bool) -> Void = { _ in } // Add this callback
+) -> SCDWidgetsContainer {
+    let container = SCDWidgetsContainer()
+    let label = EasySCDTextLabel(text: text, fontsize: 20, font: "ArialMT", fontcolor: fontColor, paddingVertical: 0, paddingHorizontal: 10, x_location: 0, y_location: 10)
+    let checkbox = EasySCDCheckbox(checked: checked, height: height)
+    checkbox.location = SCDGraphicsPoint(x: Int(screenInfo.screenSize.width) - height - 20, y: 0)
+    container.children.append(label)
+    container.children.append(checkbox)
+    container.size = SCDGraphicsDimension(width: Int(screenInfo.screenSize.width), height: height)
+    container.location = SCDGraphicsPoint(x: 0, y: 0)
+
+    // Setup click action for container
+    container.onClick { _ in action() }
+    
+    // Add an onChange listener to the checkbox to handle checked changes
+    checkbox.onClick { _ in
+		onCheckedChanged(checkbox.checked)
+    }
+
+    return container
+}
+
 
 // creates a Checkbox form
 public func EasySCDCheckboxForm(elements: [SCDWidgetsContainer]) -> SCDWidgetsContainer
