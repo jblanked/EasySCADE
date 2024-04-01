5ee3bee5cf5ca0036f1679c629ca0e9a78f1c3fc
jblanked <82678820+jblanked@users.noreply.github.com>
2024-04-01 16:10:18 -0400

    Update EasySCADE.swift

--- Sources/EasySCADE/EasySCADE.swift
+++ Sources/EasySCADE/EasySCADE.swift
@@ -1403,8 +1403,7 @@ public func EasySCDNavigationBar(
   {
   	let navbar = SCDWidgetsNavigationBar()
   	navbar.location = SCDGraphicsPoint(x: 0, y: Int(screenInfo.statusBarsize.height))
-  	navbar.size = SCDGraphicsDimension(width: Int(screenInfo.screenSize.width), height:  20)
-
+	
   	navbar.backgroundColor = backgroundColor
   	navbar.name = "SCDNavigationBar"
   	
@@ -1420,7 +1419,13 @@ public func EasySCDNavigationBar(
 					action: {
 						action()
 					})
-	label.size = SCDGraphicsDimension(width: Int(screenInfo.screenSize.width), height:  40)		
+	#if os(iOS)
+  	navbar.size = SCDGraphicsDimension(width: Int(screenInfo.screenSize.width), height: Int(screenInfo.statusBarsize.height) + 10)
+	label.size = SCDGraphicsDimension(width: Int(screenInfo.screenSize.width), height: Int(screenInfo.statusBarsize.height) + 10)	
+	#else
+	label.size = SCDGraphicsDimension(width: Int(screenInfo.screenSize.width), height: 20)	
+	navbar.size = SCDGraphicsDimension(width: Int(screenInfo.screenSize.width), height: 20)
+	#endif	
   	navbar.children = [label]
   	
   	navbar.onClick { _ in
