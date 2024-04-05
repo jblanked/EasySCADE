569c1b50d0750e8e2e8fb03f0039ba07d66ebf04
jblanked <82678820+jblanked@users.noreply.github.com>
2024-04-05 14:39:16 -0400

    Update EasySCADE.swift

--- Sources/EasySCADE/EasySCADE.swift
+++ Sources/EasySCADE/EasySCADE.swift
@@ -1187,7 +1187,7 @@ public func EasySCDTextLabel(_ text: String,
         
 		let stack = EasyVStack(elements, location: SCDGraphicsPoint(x: x_location, y: y_location))
 		stack.onClick { _ in action() }
-		stack.size = SCDGraphicsDimension(width: Int(screenInfo.screenSize.width), height: Int(count * fontsize * 1.5) + paddingVertical)
+		stack.size = SCDGraphicsDimension(width: Int(screenInfo.screenSize.width), height: Int(count * fontsize) + paddingVertical)
         return stack
     }
 
