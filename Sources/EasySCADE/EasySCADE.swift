c0ec7e7dd534285d15d7f428ad18265d648c2df5
jblanked <82678820+jblanked@users.noreply.github.com>
2024-05-30 19:57:30 -0400

    Update EasySCADE.swift

--- Sources/EasySCADE/EasySCADE.swift
+++ Sources/EasySCADE/EasySCADE.swift
@@ -564,7 +564,7 @@ public func EasySwipeGesture(
 	}
 // add the gesture to the widget after the widget has been added to the page so that the recognizer will use the entire length of the widget instead of just the original page size
 public func EasySwipeGesture(
-		_ widget: SCDWidgetsWidget,
+		  widget: SCDWidgetsWidget,
 		_ direction: SCDSvgSwipeDirection = .left,
 		_ action: @escaping () -> Void = { print("swiped") }
 		)  {
@@ -1809,6 +1809,53 @@ public func EasySCDNavigationBar(
 		backArrow.y = SCDSvgUnit(integerLiteral: 0) 
   	
   	return navbar
+  }
+
+public func EasySCDNavigationBar(
+		page: SCDWidgetsPage,
+		backgroundColor: SCDGraphicsRGB = EasyColor.white,
+		action: @escaping () -> Void = { },
+		fontcolor: SCDGraphicsRGB = EasyColor.blue
+		) 
+  {
+  	let navbar = SCDWidgetsNavigationBar()
+  	navbar.location = SCDGraphicsPoint(x: 0, y: 0)
+	
+  	navbar.backgroundColor = backgroundColor
+  	navbar.name = "SCDNavigationBar"
+  	
+  	let label = EasySCDTextLabel(
+    		 		"Back", 
+    		 		fontsize: 20,
+    		 		font: "ArialMT",
+					fontcolor:fontcolor,
+					paddingVertical:  0,
+					paddingHorizontal: 0,
+					x_location: Int(screenInfo.screenSize.width / 11),
+					y_location: -3,
+					action: {
+						action()
+					})
+	label.size = SCDGraphicsDimension(width: Int(screenInfo.screenSize.width), height: 40)	
+	navbar.size = SCDGraphicsDimension(width: Int(screenInfo.screenSize.width), height: 20)
+	
+  	navbar.children = [label]
+  	
+  	navbar.onClick { _ in
+  		
+  		action()
+
+  	}
+  	
+  	let backArrow = navbar.drawing!.findByAttribute("template-id", value: "back-arrow-node") as! SCDSvgBox
+		backArrow.fill = EasyRGBColorToSVG(fontcolor)		
+		backArrow.x = SCDSvgUnit(integerLiteral: 10) 	
+		backArrow.y = SCDSvgUnit(integerLiteral: 0) 
+	
+	// apply swipe gesture to the navbar
+	EasySwipeGesture(page, .left, action) 
+  	
+  	page.children.append(navbar)
   }
 
 public struct EasySCDToolbarItem
