5958ba6a06c651a47aecda997f0300da72e1bda5
jblanked <82678820+jblanked@users.noreply.github.com>
2024-06-07 22:22:00 -0400

    Arrange files

---
+++ Sources/Extensions.swift
@@ -0,0 +1,163 @@
+// Import necessary modules and conditionally import platform-specific ones
+import ScadeKit
+import Dispatch
+import Foundation
+import ScadeUI
+
+#if os(iOS)
+import UIKit 
+import SwiftUI
+#endif
+
+#if os(Android)
+import AndroidView
+import Java
+import Android
+import AndroidApp
+import AndroidContent
+import AndroidOS
+#endif
+
+extension SCDLatticePageAdapter {
+
+    // add widgets to the current page
+	public func append(_ widget: SCDWidgetsWidget) {
+		self.page?.children.append(widget)
+	}
+	
+    // get the screen size
+	public var screenSize: CGSize {
+		return CGSize(width: screenInfo.screenSize.width, height: screenInfo.screenSize.height)
+	}
+
+    // A method to lay out EasyVStack on the current page
+    public func vStack(@WidgetArrayBuilder _ builder: () -> [SCDWidgetsWidget], _ spacing: Int = 10) {
+        let st = EasyVStack.init(spacing: spacing, widgets: builder())
+        st.layout(in: self.page!)
+    }    
+
+	// A method to lay out EasyHStack on the current page
+	public func hStack(@WidgetArrayBuilder _ builder: () -> [SCDWidgetsWidget], _ spacing: Int = 10) {
+		let st = EasyHStack.init(spacing: spacing, widgets: builder())
+		st.layout(in: self.page!)
+	}
+
+    // method to navigate to a page
+	public func navigateTo(_ page: String = "main.page") {
+		DispatchQueue.main.async { self.navigation?.go(page: page) }
+	}
+
+    // add scroll bar to the current page
+	public func scrollbar(
+			_ onScroll: @escaping () -> Void = { },
+			onPageEnter: @escaping () -> Void = { },
+			onPageExit: @escaping () -> Void = { }
+		) {
+		 EasySCDScrollbar(
+			self.page!,
+			self.page!,
+			onScroll,
+			onPageEnter: onPageEnter,
+			onPageExit: onPageExit
+		)	
+		
+	}
+
+    // add swipe gesture to the current page
+	public func swipeGesture(
+			_ action: @escaping () -> Void = {  },
+			_ direction: SCDSvgSwipeDirection = .right
+		)  {
+			EasySwipeGesture(self.page!, direction, action)
+		}
+
+	// a method to set a page as a page
+	public func setTemplate(_ template: SCDWidgetsPage) {
+		self.page!.children = template.children
+		self.page!.onEnter = template.onEnter
+		self.page!.onExit = template.onExit
+		self.page!.useSafeArea = template.useSafeArea
+		self.page!.backgroundColor = template.backgroundColor
+		self.page!.backgroundImage = template.backgroundImage
+		self.page!.contentSize = template.contentSize
+		self.page!.cornerRadius = template.cornerRadius
+		self.page!.enable = template.enable
+		self.page!.layout = template.layout
+		self.page!.layoutData = template.layoutData
+		self.page!.location = template.location
+		self.page!.maxArea = template.maxArea
+		self.page!.minArea = template.minArea
+		self.page!.name = template.name
+		self.page!.onClick = template.onClick
+		self.page!.paddingBottom = template.paddingBottom
+		self.page!.paddingTop = template.paddingTop
+		self.page!.paddingLeft = template.paddingLeft
+		self.page!.paddingRight = template.paddingRight
+		self.page!.size = template.size
+		self.page!.tag = template.tag
+		self.page!.visible = template.visible
+	}
+
+}
+// extension to SCDWidgetsWidget to add a swipe gesture
+extension SCDWidgetsWidget {
+	public func swipeGesture(
+			_ action: @escaping () -> Void = {  },
+			_ direction: SCDSvgSwipeDirection = .right
+		)  {
+			EasySwipeGesture(self, direction, action)
+		}
+}
+
+extension SCDWidgetsContainer {
+	// A method to lay out EasyVStack in a container
+	public func vStack(@WidgetArrayBuilder _ builder: () -> [SCDWidgetsWidget], _ spacing: Int = 10) {
+		let st = EasyVStack.init(spacing: spacing, widgets: builder())
+		st.layout(in: self)
+	}
+
+	// A method to lay out EasyHStack in a container
+	public func hStack(@WidgetArrayBuilder _ builder: () -> [SCDWidgetsWidget], _ spacing: Int = 10) {
+		let st = EasyHStack.init(spacing: spacing, widgets: builder())
+		st.layout(in: self)
+	}
+}
+
+
+// extension to SCDGraphicsRGB to make it Equatable
+extension SCDGraphicsRGB: Equatable {
+    public static func ==(lhs: SCDGraphicsRGB, rhs: SCDGraphicsRGB) -> Bool {
+        return lhs.red == rhs.red && lhs.green == rhs.green && lhs.blue == rhs.blue
+    }
+}
+
+// extension to SCDSvgRGBColor to make it Equatable
+extension SCDSvgRGBColor: Equatable {
+    public static func ==(lhs: SCDSvgRGBColor, rhs: SCDSvgRGBColor) -> Bool {
+        return lhs.red == rhs.red && lhs.green == rhs.green && lhs.blue == rhs.blue
+    }
+}
+
+// change the text of an EasySCDTextLabel
+extension SCDWidgetsContainer {
+	
+    public func setText(_ text: String) {
+        if let tempContainer = self.children.first as? SCDWidgetsContainer,
+           let label = tempContainer.children.first as? SCDWidgetsLabel {
+            label.text = text
+        }
+    }
+}
+
+#if os(iOS) 
+// extension to SCDWidgetsContainer to add a SwiftUI view
+extension SCDApplication {
+	
+	static var rootViewController : UIViewController? {
+		
+		get {
+			return UIApplication.shared.delegate?.window??.rootViewController
+		}
+	}
+}
+#endif
---
+++ Sources/Templates.swift
@@ -0,0 +1,271 @@
+// Import necessary modules and conditionally import platform-specific ones
+import ScadeKit
+import Dispatch
+import Foundation
+import ScadeUI
+
+#if os(iOS)
+import UIKit 
+import SwiftUI
+#endif
+
+#if os(Android)
+import AndroidView
+import Java
+import Android
+import AndroidApp
+import AndroidContent
+import AndroidOS
+#endif
+
+public class EasyTemplateRegistration {
+	public var username: String  = ""
+    public var password: String = "" 
+    public var password2: String = ""
+    public var firstName: String = ""
+    public var lastName: String = ""
+    public var email: String = "" 
+    public var onClick: () -> Void = { 
+			print("")
+			print("Register Button Clicked") 
+			print("")
+		}
+
+
+	private func validate() -> Bool {
+		if (username.isEmpty || password.isEmpty || password2.isEmpty || firstName.isEmpty || lastName.isEmpty || email.isEmpty) {
+			EasyAlert(title: "Registration Error", message: "Please fill in all fields.")
+			return false
+		}
+		if (password != password2) {
+			EasyAlert(title: "Registration Error", message: "Passwords do not match.")
+			return false
+		}
+		return true
+	}
+	
+	public init	(
+			_ page: SCDLatticePageAdapter,
+			_ formNumber: Int = 1,
+			name: String = "easyTemplateRegistration", 
+			backgroundColor: SCDGraphicsRGB = EasyColor.white
+		) {
+		
+		let userTextbox = EasySCDTextBox("Your Username", secure: false)
+   		userTextbox.onTextChange.append(SCDWidgetsTextChangeEventHandler { ev in self.username = ev!.newValue })
+   		
+   		let emailTextbox = EasySCDTextBox("Your Email", secure: false)
+   		emailTextbox.onTextChange.append(SCDWidgetsTextChangeEventHandler { ev in self.email = ev!.newValue })
+   		
+   		let firstnameTextbox = EasySCDTextBox("Your First Name", secure: false)
+   		firstnameTextbox.onTextChange.append(SCDWidgetsTextChangeEventHandler { ev in self.firstName = ev!.newValue })
+   
+   		let lastnameTextbox = EasySCDTextBox("Your Last Name", secure: false)
+   		lastnameTextbox.onTextChange.append(SCDWidgetsTextChangeEventHandler { ev in self.lastName = ev!.newValue })
+   		
+   		let passTextbox = EasySCDTextBox("Your Password", secure: true)
+   		passTextbox.onTextChange.append(SCDWidgetsTextChangeEventHandler { ev in self.password = ev!.newValue })
+   
+   		let pass2Textbox = EasySCDTextBox("Your Password Again", secure: true)
+   		pass2Textbox.onTextChange.append(SCDWidgetsTextChangeEventHandler { ev in self.password2 = ev!.newValue })
+   		
+   		let buttonRegister: SCDWidgetsButton = EasySCDButton("Register", font: "ArialMT",color: EasyColor.blue)
+   		
+   		buttonRegister.onClick( 
+   			{	
+   				_ in 
+
+				if self.validate() {
+					self.onClick()
+				}
+   			}
+   		)
+
+		var temp: SCDWidgetsPage = SCDWidgetsPage()		
+   
+		if formNumber == 1 {
+   		
+		temp  = EasySCDPage(
+			name: name,
+			children: [
+				EasySCDVStack([
+				EasySCDTextForm(
+    		
+    				[
+    			
+    				EasySCDTextBoxForm	(
+    					title: "Username",
+    					textbox: userTextbox				
+    				),	
+    				EasySCDTextBoxForm	(
+    					title: "Email",
+    					textbox: emailTextbox  				
+    				),
+    				EasySCDTextBoxForm	(
+    					title: "First Name",
+    					textbox: firstnameTextbox  				
+    				),
+    				EasySCDTextBoxForm	(
+    					title: "Last Name",
+    					textbox: lastnameTextbox  				
+    				),
+    				EasySCDTextBoxForm	(
+    					title: "Password",
+    					textbox: passTextbox  				
+    				),
+    				EasySCDTextBoxForm	(
+    					title: "Password (Again)",
+    					textbox: pass2Textbox  				
+    				),
+    				
+    				
+    			
+    			],
+    			
+    			fontsize: 20,
+				font: "ArialMT", 
+				fontcolor: EasyColor.black
+    		),
+    		buttonRegister
+    		])
+    		
+			],
+			useSafeArea: false,
+			backgroundColor: backgroundColor,
+			onEnter: { },
+			onExit: { }
+		)
+
+		}
+		else if formNumber == 2
+		{
+			temp  = EasySCDPage(
+			name: name,
+			children: [
+				EasySCDVStack([
+					userTextbox,
+					emailTextbox,
+					firstnameTextbox,
+					lastnameTextbox,
+					passTextbox,
+					pass2Textbox,
+					buttonRegister 				
+    			])	
+    		],
+			useSafeArea: false,
+			backgroundColor: backgroundColor,
+			onEnter: { },
+			onExit: { }
+		)
+		}
+		
+		page.setTemplate(temp)
+	}
+}
+
+public class EasyTemplateLogin {
+	public var username: String  = ""
+	public var password: String = "" 
+	public var onClick: () -> Void = { 
+			print("")
+			print("Sign In Button Clicked") 
+			print("")
+		}
+
+	private func validate() -> Bool {
+		if (username.isEmpty || password.isEmpty) {
+			EasyAlert(title: "Sign In Error", message: "Please fill in all fields.")
+			return false
+		}
+		return true
+	}
+	
+	public init	(
+			_ page: SCDLatticePageAdapter,
+			_ formNumber: Int = 1,
+			name: String = "easyTemplateSignIn", 
+			backgroundColor: SCDGraphicsRGB = EasyColor.white
+		) {
+		
+		let userTextbox = EasySCDTextBox("Your Username", secure: false)
+   		userTextbox.onTextChange.append(SCDWidgetsTextChangeEventHandler { ev in self.username = ev!.newValue })
+   		
+   		let passTextbox = EasySCDTextBox("Your Password", secure: true)
+   		passTextbox.onTextChange.append(SCDWidgetsTextChangeEventHandler { ev in self.password = ev!.newValue })
+   		
+   		let buttonSignIn: SCDWidgetsButton = EasySCDButton("Sign In", font: "ArialMT",color: EasyColor.blue)
+   		
+   		buttonSignIn.onClick( 
+   			{	
+   				_ in 
+
+				if self.validate() {
+					self.onClick()
+				}
+   			}
+   		)
+
+		var temp: SCDWidgetsPage = SCDWidgetsPage()	
+		
+			if formNumber == 1 {	
+   		
+		temp  = EasySCDPage(
+			name: name,
+			children: [
+				EasySCDVStack([
+				EasySCDTextForm(
+			
+					[
+				
+					EasySCDTextBoxForm	(
+						title: "Username",
+						textbox: userTextbox				
+					),	
+					EasySCDTextBoxForm	(
+						title: "Password",
+						textbox: passTextbox  				
+					),
+					
+					
+				
+				],
+				
+				fontsize: 20,
+				font: "ArialMT", 
+				fontcolor: EasyColor.black
+			),
+			buttonSignIn
+			])
+			
+			],
+			useSafeArea: false,
+			backgroundColor: backgroundColor,
+			onEnter: { },
+			onExit: { }
+		)
+		
+		}
+		
+		else if formNumber == 2
+		{
+			temp  = EasySCDPage(
+			name: name,
+			children: [
+				EasySCDVStack([
+				userTextbox,
+				passTextbox,				
+				buttonSignIn
+			])
+			
+			],
+			useSafeArea: false,
+			backgroundColor: backgroundColor,
+			onEnter: { },
+			onExit: { }
+		)
+		}
+
+		page.setTemplate(temp)
+
+		}
+}
