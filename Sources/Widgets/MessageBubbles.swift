f33ef9d154b1ba4a727b3ca4e306e8634ae10df0
jblanked <82678820+jblanked@users.noreply.github.com>
2024-06-24 08:40:32 -0400

    Update MessageBubbles.swift

--- Sources/Widgets/MessageBubbles.swift
+++ Sources/Widgets/MessageBubbles.swift
@@ -172,7 +172,7 @@ func splitTextIntoLines(_ text: String, _ fontSize: Int = 20) -> [String] {
     let components = formattedText.components(separatedBy: .newlines)
     
     // Calculate maximum letters per line based on font size and screen width
-    let averageCharacterWidth = fontSize * 0.6 // Approximate average width of a character at given font size
+    let averageCharacterWidth = Double(fontSize) * 0.6 // Approximate average width of a character at given font size
     let maxLettersPerLine = Int(screenInfo.screenSize.width / averageCharacterWidth)
 
     var lines: [String] = []
